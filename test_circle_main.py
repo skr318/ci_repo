6a74f4fb0511fa1328b82de6362026f3caf4d713
sanjev <60310531+skr318@users.noreply.github.com>
2020-05-20 16:03:54 +0530

    Update test_circle_main.py

--- test_circle_main.py
+++ test_circle_main.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pytest
+import os
 
 # chromedriver = "/"
+os.chmod('driver/chromedriver', 0755) # e.g. os.chmod('/Users/user/Documents/my_project/chromedriver', 0755)
 driver = webdriver.Chrome(executable_path='driver/chromedriver')
 driver.set_window_size(1024, 600)
 # driver.fullscreen_window()
