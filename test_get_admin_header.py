cec1e5d47595e75cfef41056896f095242a358ab
sanjev <60310531+skr318@users.noreply.github.com>
2020-05-20 21:05:58 +0530

    Update test_get_admin_header.py

--- test_get_admin_header.py
+++ test_get_admin_header.py
@@ -2,17 +2,19 @@ import requests
 import json
 
 
-url = "https://sanjev.supporthive.com/api/v2/staff-login/"
-username = "sanjev.k@happyfox.com"
-password = "Qwert12345!"
-payload = {
-	"username":username,
-	"password":password
-	}
+class TestAdminAuth:
 
-def test_staff_auth():
-	staff_login = requests.request("POST", url, data = payload).text.encode('utf8')
-	staff_login_dict = json.loads(staff_login)
-	staff_auth = staff_login_dict['auth_token']
-	assert len(staff_auth) > 0
-	assert len(staff_login_dict['refresh_token']) > 0
+	url = "https://sanjev.supporthive.com/api/v2/staff-login/"
+	username = "sanjev.k@happyfox.com"
+	password = "Qwert12345!"
+	payload = {
+		"username":username,
+		"password":password
+		}
+	@pytest.mark.hotfix
+	def test_staff_auth():
+		staff_login = requests.request("POST", url, data = payload).text.encode('utf8')
+		staff_login_dict = json.loads(staff_login)
+		staff_auth = staff_login_dict['auth_token']
+		assert len(staff_auth) > 0
+		assert len(staff_login_dict['refresh_token']) > 0
