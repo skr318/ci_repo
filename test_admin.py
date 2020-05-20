95169247a124e915a0413ffadd8a04e2683005bc
sanjev <60310531+skr318@users.noreply.github.com>
2020-05-20 21:20:33 +0530

    Update test_admin.py

--- test_admin.py
+++ test_admin.py
@@ -15,7 +15,7 @@ class TestAdmin:
 	@pytest.mark.sanity
 	def test_staff_auth(self):
 		self.staff_login = requests.request("POST", self.url, data = self.payload).text.encode('utf8')
-		self.staff_login_dict = json.loads(staff_login)
+		self.staff_login_dict = json.loads(self.staff_login)
 		self.staff_auth = self.staff_login_dict['auth_token']
 		assert len(self.staff_auth) > 0
 		assert len(self.staff_login_dict['refresh_token']) > 0
