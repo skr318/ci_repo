217b4dbbd54f13ee63164881c221f74812529936
sanjev <60310531+skr318@users.noreply.github.com>
2020-05-20 21:15:36 +0530

    Update and rename test_get_admin_header.py to test_admin_auth.py

---
+++ test_admin_auth.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import pytest
+
+
+class TestAdminAuth:
+
+	url = "https://sanjev.supporthive.com/api/v2/staff-login/"
+	username = "sanjev.k@happyfox.com"
+	password = "Qwert12345!"
+	payload = {
+		"username":username,
+		"password":password
+		}
+	
+	
+	@pytest.mark.hotfix
+	def test_staff_auth(self):
+		self.staff_login = requests.request("POST", url, data = payload).text.encode('utf8')
+		self.staff_login_dict = json.loads(self.staff_login)
+		self.staff_auth = self.staff_login_dict['auth_token']
+		assert len(self.staff_auth) > 0
+		assert len(self.staff_login_dict['refresh_token']) > 0
