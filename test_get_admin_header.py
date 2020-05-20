b1a41de6c7bafc6702752e9cd5a661e8ba73fda4
sanjev <60310531+skr318@users.noreply.github.com>
2020-05-20 14:51:13 +0530

    testing BE

---
+++ test_get_admin_header.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+
+url = "https://sanjev.supporthive.com/api/v2/staff-login/"
+username = "sanjev.k@happyfox.com"
+password = "Qwert12345!"
+staff_login = requests.request("POST", url, data = payload).text.encode('utf8')
+staff_login_dict = json.loads(staff_login)
+staff_auth = staff_login_dict['auth_token']
+print(staff_auth)
