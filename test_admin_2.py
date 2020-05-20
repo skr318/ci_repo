49f469c8374b9ecf3ad93ceabcd274cdb78dc3b3
sanjev <60310531+skr318@users.noreply.github.com>
2020-05-20 20:58:00 +0530

    Update test_admin_2.py

--- test_admin_2.py
+++ test_admin_2.py
@@ -1,6 +1,19 @@
+import requests
 import json
-import pytest
+
+
+url = "https://sanjev.supporthive.com/api/v2/staff-login/"
+username = "sanjev.k@happyfox.com"
+password = "Qwert12345!"
+payload = {
+	"username":username,
+	"password":password
+	}
 
 @pytest.mark.sanity
-def test_auth():
-    pass
+def test_staff_auth():
+	staff_login = requests.request("POST", url, data = payload).text.encode('utf8')
+	staff_login_dict = json.loads(staff_login)
+	staff_auth = staff_login_dict['auth_token']
+	assert len(staff_auth) > 0
+	assert len(staff_login_dict['refresh_token']) > 0
